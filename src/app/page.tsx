ff82e4bca11f9d8ab58a22a71f07ffa0ae92b673
raj-umang <jackerumang@gmail.com>
2024-03-19 01:30:32 +0530

    Testimonials & Webinars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonial from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/ui/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonial />
+      <UpcomingWebinars />
     </main>
   );
 }
--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import courseData from "../data/music_courses.json";
 import { BackgroundGradient } from "./ui/background-gradient";
+import { Button } from "./ui/moving-border";
 
 interface Course {
   id: number;
@@ -50,11 +51,13 @@ function FeaturedCourses() {
         </div>
       </div>
       <div className="mt-20 text-center">
-        <Link
-          href={"/courses"}
-          className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
-        >
-          View All courses
+        <Link href={"/courses"} className="">
+          <Button
+            borderRadius="1.75rem"
+            className="bg-gray-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+          >
+            View All courses
+          </Button>
         </Link>
       </div>
     </div>
